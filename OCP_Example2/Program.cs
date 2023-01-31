aa75d183788825437f95d343238f6e6f74a440ae
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-01-31 20:51:42 +0100

    OCP Second Project Example

---
+++ OCP_Example2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(2.0,3.0);
+
+            Shape square = new Square(4);
+
+            Console.WriteLine($"Area of rectangle is: {rectangle.GetShapeArea()}");
+            Console.WriteLine($"Area of square is: {square.GetShapeArea()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OCP_Example2/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Example2
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+
+        public double Length { get; set; }
+
+        public Rectangle(double width, double length)
+        {
+            Width = width;
+            Length = length;
+        }
+
+        public override double GetShapeArea()
+        {
+            return Width * Length;
+        }
+    }
+}
+ 
